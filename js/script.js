e5f95ef5abba9f8f7722e3d26653017748f053f6
Alexandr <diegopwnz@gmail.com>
2021-05-02 19:36:23 +0400

    typatop

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 "use strict";
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let lastFilm = prompt('Один из проследних просмотренных фильмов?');
+let filmRate = +prompt('На сколько оцените его?');
+
+
+let personalMovieDB = {
+	count: numberOfFilms,
+	movies: { lastFilm, filmRate
+},
+	actors: '',
+	genres: [],
+	privat: false
+};
+
+console.log(personalMovieDB.movies);
