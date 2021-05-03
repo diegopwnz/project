90e14b0844893e7990a3a66328f5a7cd42e28d09
Alexandr <diegopwnz@gmail.com>
2021-05-03 20:37:56 +0400

    zada4a

--- js/script.js
+++ js/script.js
@@ -1,16 +1,37 @@
 "use strict";
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-let lastFilm = prompt('Один из проследних просмотренных фильмов?');
-let filmRate = +prompt('На сколько оцените его?');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-
-let personalMovieDB = {
+const personalMovieDB = {
 	count: numberOfFilms,
-	movies: { lastFilm, filmRate
-},
-	actors: '',
+	movies: {},
+	actors: {},
 	genres: [],
 	privat: false
 };
 
-console.log(personalMovieDB.movies);
+
+
+for (let i = 0; i < 2; i++) {
+	const a = prompt('Один из последних просмотренных фильмов?', ''),
+		b = prompt('На сколько оцените его?', '');
+	if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+		personalMovieDB.movies[a] = b;
+		console.log('done');
+	} else {
+		console.log('error');
+		i--;
+	}
+
+}
+
+if (personalMovieDB.count < 10) {
+	console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+	console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+	console.log("Вы киноман");
+} else {
+	console.log("Произошла ошибка");
+}
+
+console.log(personalMovieDB);
